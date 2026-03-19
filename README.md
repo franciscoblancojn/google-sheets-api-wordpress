4c9fc84653f70a568e50bc1dd77b49b401dd0144
Francisco Blanco <blancofrancisco34@gmail.com>
2026-03-19 18:46:31 -0500

    iniit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Google Sheets Api
+
+Contributors: Francisco Blanco
+
+Tags: sheets, google sheets
+
+Requires at least: 5.0
+
+Tested up to: 5.9
+
+Stable tag: 1.0.0
+
+License: GPLv2 or later
+
+License URI: [https://www.gnu.org/licenses/gpl-2.0.html](https://www.gnu.org/licenses/gpl-2.0.html)
+
+It is an plugin of wordpress, for send information to google sheets
+
+## Description
+
+### Installation
+
+1. Download .zip [__CLICK HERE__](https://github.com/franciscoblancojn/google-sheets-api-wordpress/archive/refs/heads/master.zip) in repository [https://github.com/franciscoblancojn/google-sheets-api-wordpress](https://github.com/franciscoblancojn/google-sheets-api-wordpress)
+2. Go to admin of site wordpress, in __Plugins -> Add New__
+3. Click in button __Upload Plugin__
+4. Click in buttom __Select File__, and Upload file
+5. Active Plugin
+
+### Configure Plugin
+
+
+## Developer
+
+* Name: Francisco Blanco
+* Website: https://franciscoblanco.vercel.app/
+* Email: blancofrancisco34@gmail.com
+
+## Repositories
+
+* GitHub: https://github.com/franciscoblancojn/google-sheets-api-wordpress
+
+## License
+This plugin is distributed under the terms of the GNU General Public License v2.0 or later.
+
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Plugin Name: Google Sheets Api
+Plugin URI: https://github.com/franciscoblancojn/google-sheets-api-wordpress
+Description: It is an plugin of wordpress, for send information to google sheets.
+Version: 1.0.0
+Author: franciscoblancojn
+Author URI: https://franciscoblanco.vercel.app/
+License: GPL2+
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wc-google-sheets-api-wordpress
+*/
+
+if (!function_exists( 'is_plugin_active' ))
+    require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+
+//GOSHAP_
+define("GOSHAP_KEY",'GOSHAP');
+define("GOSHAP_LOG",true);
+define("GOSHAP_LOG_COUNT",100);
+define("GOSHAP_BASENAME",plugin_basename(__FILE__));
+define("GOSHAP_DIR",plugin_dir_path( __FILE__ ));
+define("GOSHAP_URL",plugin_dir_url(__FILE__));
+
+require_once GOSHAP_DIR . 'update.php';
+github_updater_plugin_wordpress([
+    'basename'=>GOSHAP_BASENAME,
+    'dir'=>GOSHAP_DIR,
+    'file'=>"index.php",
+    'path_repository'=>'franciscoblancojn/google-sheets-api-wordpress',
+    'branch'=>'master',
+    'token_array_split'=>[
+        "g",
+        "h",
+        "p",
+        "_",
+        "G",
+        "4",
+        "W",
+        "E",
+        "W",
+        "F",
+        "p",
+        "V",
+        "U",
+        "E",
+        "F",
+        "V",
+        "x",
+        "F",
+        "U",
+        "n",
+        "b",
+        "M",
+        "k",
+        "P",
+        "R",
+        "x",
+        "o",
+        "f",
+        "t",
+        "Y",
+        "8",
+        "z",
+        "j",
+        "t",
+        "4",
+        "E",
+        "x",
+        "b",
+        "i",
+        "9"
+    ]
+]);
+
